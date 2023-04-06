122e8a3bc00c5fb8bcebd126545adb4f531d3d4c
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 01:52:07 -0700

    Fixed header bug

--- src/components/Footer_Navigation/FooterNavigation.js
+++ src/components/Footer_Navigation/FooterNavigation.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import FolderIcon from "@mui/icons-material/Folder";
+import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PersonIcon from "@mui/icons-material/Person";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function FooterNavigation() {
   const [value, setValue] = React.useState("Home");
@@ -14,27 +16,28 @@ export default function FooterNavigation() {
   };
 
   return (
-    <BottomNavigation className='sticky bottom-0 w-screen' value={value} onChange={handleChange}>
+    <BottomNavigation
+      className="sticky bottom-0 w-screen"
+      value={value}
+      onChange={handleChange}
+    >
+      <BottomNavigationAction label="Home" value="Home" icon={<HomeIcon />} />
       <BottomNavigationAction
-        label="Home"
-        value="Home"
-        icon={<HomeIcon />}
+        label="Profile"
+        value="profile"
+        icon={<PersonIcon />}
       />
       <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
+        label="My Cart"
+        value="cart"
+        icon={<LocalMallIcon />}
       />
       <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
+        label="Search"
+        value="search"
+        icon={<SearchIcon />}
       />
-      <BottomNavigationAction
-        label="Folder"
-        value="folder"
-        icon={<FolderIcon />}
-      />
+      <BottomNavigationAction label="Menu" value="menu" icon={<MenuIcon />} />
     </BottomNavigation>
   );
 }
