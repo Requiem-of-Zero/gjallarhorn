d769a6228e999257de9d70ecda7fb26f14170d44
requiem-of-zero <theesamwong@gmail.com>
2023-04-05 19:28:31 -0700

    first

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div className="bg-[#000000] w-screen h-20 flex justify-center items-center py-2">
-      <h1 className="text-3xl text-[#87CEEB]">NEW TYPE OCEAN</h1>
+      <h1 className="text-3xl text-blue">NEW TYPE OCEAN</h1>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header/Header'
+import Banner from '@/components/Banner/Banner'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <main>
         <Header />
+        <Banner />
       </main>
     </>
   )
