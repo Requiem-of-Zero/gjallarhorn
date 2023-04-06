6ba90f8b4ede3f4e514a75bca65ff1338b1258ec
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 00:15:40 -0700

    Added product logic to track inventory

--- src/components/ProductTile/ProductTile.js
+++ src/components/ProductTile/ProductTile.js
@@ -1,11 +1,30 @@
-const ProductTile = ({name, description, imgUrl, price, quantity}) => {
+const ProductTile = ({ name, description, imgUrl, price, quantity }) => {
+  const handleQuantity = (quantity) => {
+    if (quantity > 20) {
+      return "In stock";
+    } else if (quantity < 10) {
+      return "< 10 left";
+    } else {
+      return "Sold out!";
+    }
+  };
   return (
     <li>
       <img src={imgUrl} alt={description} />
-      <h3>{description}</h3>
-      <h2>{name}</h2>
+      <h3 className="text-light-grey">{description}</h3>
+      <h2 className="text-white">{name}</h2>
+      <div className="flex justify-between">
+        <p className="text-light-grey">{"$" + price}</p>
+        <p
+          className={`px-2 ${
+            quantity > 20 ? "bg-[green] text-white" : "text-grey bg-[#F7C00B]"
+          }`}
+        >
+          {handleQuantity(quantity)}
+        </p>
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default ProductTile
+export default ProductTile;
