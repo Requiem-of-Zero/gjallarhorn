f30a66b24bb56d7fdd1f40bcc7c8015e3fb7e713
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 01:54:57 -0700

    Fixed spacing for products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,7 @@ import ProductTile from "../ProductTile/ProductTile";
 const Products = ({ products }) => {
   return (
     <div>
-      <ul className="grid grid-cols-2 gap-[5px] px-2 py-2">
+      <ul className="grid grid-cols-2 gap-[5px] px-2 pt-2 pb-5">
         {products.map((product, i) => (
           <ProductTile key={`product-${i}`} {...product} />
         ))}
