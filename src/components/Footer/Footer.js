f4f1df54adfb7b5f99388b0b52897a3de21003ef
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 01:07:21 -0700

    Completed mobile footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,36 @@
-import { AiOutlineInstagram, AiFillRedditCircle } from "react-icons/ai";
+import { AiOutlineInstagram, AiFillRedditCircle, AiOutlineCopyrightCircle } from "react-icons/ai";
 import { BsFacebook, BsTwitter } from "react-icons/bs";
+import { FaMoon } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-grey">
-      <ul className="text-light-grey pl-3">
+      <ul className="text-light-grey pl-3 pt-4">
         <li>Information</li>
         <li>About Us</li>
         <li>Help</li>
         <li>Terms of Use</li>
         <li>Privacy Policy</li>
-        <li>Socials</li>
-        <ul id='footer-socials' className="flex gap-[10px]">
-          <li>
-            <AiOutlineInstagram size={20} />
-          </li>
-          <li>
-            <BsFacebook size={20} />
-          </li>
-          <li>
-            <BsTwitter size={20} />
-          </li>
-          <li>
-            <AiFillRedditCircle size={20} />
-          </li>
-        </ul>
-
-        <li className="text-sm">
-          NEW TYPE OCEAN LLC/INC 2023 All Rights Reserved
+        <div className="py-2">
+          Socials
+          <ul id="footer-socials" className="flex gap-[10px]">
+            <li>
+              <AiOutlineInstagram size={20} />
+            </li>
+            <li>
+              <BsFacebook size={20} />
+            </li>
+            <li>
+              <BsTwitter size={20} />
+            </li>
+            <li>
+              <AiFillRedditCircle size={20} />
+            </li>
+          </ul>
+        </div>
+        <div id="light-dark-mode" className='flex gap-[8px]'>Mode <FaMoon /></div>
+        <li className="text-sm flex gap-[5px]">
+          <AiOutlineCopyrightCircle /> NEW TYPE OCEAN LLC/INC 2023 All Rights Reserved
         </li>
       </ul>
     </footer>
