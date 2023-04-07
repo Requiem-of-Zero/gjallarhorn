0418bb79af278621ce048b328aa74b5f038e7cd4
requiem-of-zero <swonggzz@gmail.com>
2023-04-07 05:44:35 -0700

    Fixed filter no results show

--- src/components/404/EmptyResults.js
+++ src/components/404/EmptyResults.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const EmptyResults = () => {
   return (
-    <div>
+    <div className="w-screen px-[11.3rem]">
       <Image
         src="https://staticg.sportskeeda.com/editor/2022/07/31987-16590245387151-1920.jpg"
         width={1920}
