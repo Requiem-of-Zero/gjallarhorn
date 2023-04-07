1a5db76ca396179c7e0a41dac0e9a77f2fe15c04
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 23:35:20 -0700

    Fixed product nav underline bug

--- src/components/Product_Navigation/ProductNavigation.js
+++ src/components/Product_Navigation/ProductNavigation.js
@@ -12,34 +12,42 @@ const ProductNavigation = ({ setActive, active, setFiltered, products }) => {
 
   return (
     <div className="m0a bg-grey text-grey scrollbar-hide z-10 flex w-full whitespace-nowrap border-b border-t border-solid overflow-x-scroll overflow-y-hidden top-header lg:top-header-desktop sticky top-[38px] max-w-contentContainer sml:justify-center w-full lgl:top-[119px]">
-      <ul className="flex gap-[30px] px-[20px] tracking-widest text-white font-[100] md:items-center md:justify-center">
+      <ul className="flex gap-[30px] px-[20px] pt-2 tracking-widest text-white font-[100] md:items-center md:justify-center">
         <li
           onClick={() => setActive(0)}
-          className={`py-2 ${active === 0 ? "border-b-4 border-blue" : ""}`}
+          className={`py-2 border-b-4 ${active === 0 ? "border-blue" : "border-[transparent]"}`}
         >
           ALL
         </li>
         <li
           onClick={() => setActive(1)}
-          className={`py-2 ${active === 1 ? "border-b-4 border-blue" : ""}`}
+          className={`py-2 border-b-4 ${
+            active === 1 ? "border-blue" : "border-[transparent]"
+          }`}
         >
           SOLD OUT
         </li>
         <li
           onClick={() => setActive(2)}
-          className={`py-2 ${active === 2 ? "border-b-4 border-blue" : ""}`}
+          className={`py-2 border-b-4 ${
+            active === 2 ? "border-blue" : "border-[transparent]"
+          }`}
         >
           LOBSTERS
         </li>
         <li
           onClick={() => setActive(3)}
-          className={`py-2 ${active === 3 ? "border-b-4 border-blue" : ""}`}
+          className={`py-2 border-b-4 ${
+            active === 3 ? "border-blue" : "border-[transparent]"
+          }`}
         >
           CRABS
         </li>
         <li
           onClick={() => setActive(4)}
-          className={`py-2 ${active === 4 ? "border-b-4 border-blue" : ""}`}
+          className={`py-2 border-b-4 ${
+            active === 4 ? "border-blue" : "border-[transparent]"
+          }`}
         >
           SHRIMPS
         </li>
