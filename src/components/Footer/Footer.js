d5f4e41ebf49a07045f925f4d7c359e3af8c9544
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 21:12:12 -0700

    Cleaned up footer socials

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
-import { AiOutlineInstagram, AiFillRedditCircle, AiOutlineCopyrightCircle } from "react-icons/ai";
-import { BsFacebook, BsTwitter } from "react-icons/bs";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -15,22 +15,16 @@ const Footer = () => {
           Socials
           <ul id="footer-socials" className="flex gap-[10px]">
             <li>
-              <AiOutlineInstagram size={20} />
-            </li>
-            <li>
-              <BsFacebook size={20} />
-            </li>
-            <li>
-              <BsTwitter size={20} />
-            </li>
-            <li>
-              <AiFillRedditCircle size={20} />
+              <Image src="https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png?20181027065954" height={20} width={20} />
             </li>
           </ul>
         </div>
-        <div id="light-dark-mode" className='flex gap-[8px]'>Mode <FaMoon /></div>
+        <div id="light-dark-mode" className="flex gap-[8px]">
+          Mode <FaMoon />
+        </div>
         <li className="text-sm flex gap-[5px] py-1 pb-2">
-          <AiOutlineCopyrightCircle /> NEW TYPE OCEAN LLC/INC 2023 All Rights Reserved
+          <AiOutlineCopyrightCircle /> NEW TYPE OCEAN LLC/INC 2023 All Rights
+          Reserved
         </li>
       </ul>
     </footer>
