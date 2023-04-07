6addc926d6e6c2e3cd44f83aeee38facde3abce2
requiem-of-zero <theesamwong@gmail.com>
2023-04-06 21:19:57 -0700

    Fixed social red book behavior

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,13 @@ const Footer = () => {
           Socials
           <ul id="footer-socials" className="flex gap-[10px]">
             <li>
-              <Image src="https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png?20181027065954" height={20} width={20} />
+              <a href="https://www.xiaohongshu.com/user/profile/5fe43a8f0000000001002554?xhsshare=WeixinSession&appuid=5be34a312f650f0001df5536&apptime=1680834133" target='_blank'>
+                <Image
+                  src="https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.png?20181027065954"
+                  height={20}
+                  width={20}
+                />
+              </a>
             </li>
           </ul>
         </div>
