f6ca68792cb2900570ed976eb880ed3373f7ac0e
requiem-of-zero <swonggzz@gmail.com>
2023-04-08 05:41:54 -0700

    test

--- next.config.js
+++ next.config.js
@@ -2,11 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["staticg.sportskeeda.com","upload.wikimedia.org","alaskaseafood.site", 'images4.alphacoders.com', 'trumpwallpapers.com', 'cdn.wallpapersafari.com', 'wallpapercave.com'],
+    domains: [
+      "staticg.sportskeeda.com",
+      "upload.wikimedia.org",
+      "alaskaseafood.site",
+      "images4.alphacoders.com",
+      "trumpwallpapers.com",
+      "cdn.wallpapersafari.com",
+      "wallpapercave.com",
+    ],
   },
   env: {
-    stripe_public_key: process.env.STRIPE_PUBLIC_KEY
-  }
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
