1b87c81c617f33d94da0e3b27f265b1a58871332
requiem-of-zero <swonggzz@gmail.com>
2023-04-08 03:22:35 -0700

    Removed unneccessary clog

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,7 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
   const { items, email } = req.body;
-  console.log(items)
+  
   const transformedProducts = items.map((item) => ({
     quantity: item.quantity,
     price_data: {
