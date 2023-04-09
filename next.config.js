b7d9bcdbfd340b9e5b5da3bcec0fd427283355e7
requiem-of-zero <swonggzz@gmail.com>
2023-04-09 04:05:49 -0700

    Update env

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
   },
   env: {
-    stripe_publishable_key: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripe_publishable_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
 };
 
