575f29070205921997776172b26ca5cd22363be9
requiem-of-zero <theesamwong@gmail.com>
2023-04-09 22:37:48 -0700

    Added height for UI

--- src/components/404/EmptyResults.js
+++ src/components/404/EmptyResults.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const EmptyResults = () => {
   return (
-    <div className="max-w-contentContainer">
+    <div className="max-w-contentContainer min-h-screen">
       <Image
         src="https://staticg.sportskeeda.com/editor/2022/07/31987-16590245387151-1920.jpg"
         width={1920}
