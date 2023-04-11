34b907a5c6af2a022851fa3a9200a3426fd1249e
requiem-of-zero <swonggzz@gmail.com>
2023-04-11 01:14:53 -0700

    Fully fully functioning light mode

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const SearchItem = ({ name, imgUrl }) => {
   return (
-    <div className="flex gap-[20px] bg-[#1F2122] py-2 pl-3 items-center text-white w-[400px]">
+    <div className="search_item flex gap-[20px] bg-[#1F2122] py-2 pl-3 items-center text-white w-[400px]">
       <Image src={imgUrl} width={70} height={70} priority/>
       <h2>{name}</h2>
     </div>
