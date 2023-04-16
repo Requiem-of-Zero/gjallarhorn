f5627378b8826eefc73dc01786a1a4c8599b6009
requiem-of-zero <swonggzz@gmail.com>
2023-04-15 23:57:35 -0700

    Started firebase product comments

---
+++ src/components/Products/Comments/Comments.js
@@ -0,0 +1,8 @@
+
+const Comments = () => {
+  return (
+    <div className="text-white">Here are the comments</div>
+  )
+}
+
+export default Comments
--- src/pages/show/[pid].js
+++ src/pages/show/[pid].js
@@ -10,6 +10,7 @@ import {
   handleQuantity,
 } from "../../components/Products/util/stock.util";
 import getEntryById from "../../contentful/client";
+import Comments from "../../components/Products/Comments/Comments";
 
 export async function getServerSideProps(context) {
   const product = await getEntryById(context.query.pid);
@@ -42,6 +43,7 @@ export default function ProductShow({ product, products, id }) {
         <div className="flex">
           <div className="py-5">
             <Image src={`http:${url}`} height={500} width={500} />
+            <Comments />
           </div>
           <div
             id="product_description"
