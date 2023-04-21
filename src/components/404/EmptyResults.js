fd7b26a10c5ee7fda5ee84b37e77480b7641f882
requiem-of-zero <theesamwong@gmail.com>
2023-04-21 00:36:41 -0700

    Updated empty results component logic and removed unused import

--- src/components/404/EmptyResults.js
+++ src/components/404/EmptyResults.js
@@ -1,10 +1,17 @@
-import Image from "next/image";
+import { useRouter } from "next/router";
 
 const EmptyResults = () => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
-    <div id='empty_background' className="w-screen min-h-screen flex relative items-center justify-center">
+    <div
+      id="empty_background"
+      className="w-screen min-h-screen flex relative items-center justify-center"
+    >
       <p className="text-6xl text-blue shadow-lg">
-        There are no items in your cart... 
+        {router.pathname === "/cart"
+          ? "There are no items in your cart..."
+          : "There were no matches"}
       </p>
     </div>
   );
