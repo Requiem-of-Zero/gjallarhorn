4304e5d961c87995a39b21cab74a8b1397481608
requiem-of-zero <theesamwong@gmail.com>
2023-04-21 21:11:00 -0700

    Centered product tiles for UI/UX

--- src/components/Products/ProductTile/ProductTile.js
+++ src/components/Products/ProductTile/ProductTile.js
@@ -28,7 +28,7 @@ const ProductTile = ({
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="max-w-[190px] min-w-[190px] flex flex-col justify-between"
+      className="max-w-[190px] min-w-[190px] flex flex-col justify-between m0a"
     >
       <Loading open={loading} setOpen={setLoading} />
       <Link href={`/show/${id}`}>
